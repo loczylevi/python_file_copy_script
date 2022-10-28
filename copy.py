389236055f1ad508c0be86f17a561c3f6f6480d0
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-10-28 19:13:44 +0200

    Create copy.py

---
+++ copy.py
@@ -0,0 +1,4 @@
+import shutil
+# Copy file example.txt into a new file called example_copy.txt
+# Copy file example.txt into directory test/
+shutil.copy('/home/loczylevi/CC/D22.txt', '/home/loczylevi/DD')
